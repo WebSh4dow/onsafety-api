68baad0d81454d81ffd1b3f9897fab94738a2c33
FireXtz <phantomrising@protonmail.com>
2024-07-20 16:41:41 -0300

    feature: cors filter add and clientquerys

---
+++ src/main/java/com/jarmison/dev/api/core/config/CorsFilter.java
@@ -0,0 +1,40 @@
+package com.jarmison.dev.api.core.config;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@CrossOrigin("http://localhost:4200")
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE,PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
+
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
--- src/main/java/com/jarmison/dev/api/repository/query/ClientQuerys.java
+++ src/main/java/com/jarmison/dev/api/repository/query/ClientQuerys.java
@@ -5,4 +5,5 @@ public interface ClientQuerys {
     final String VERIFY_CLIENT_CPF = "SELECT CASE WHEN COUNT(pe) > 0 THEN TRUE ELSE FALSE END FROM PersonEntity pe WHERE pe.email = ?1";
 
     final String VERIFY_CLIENT_EMAIL = "SELECT CASE WHEN COUNT(pe) > 0 THEN TRUE ELSE FALSE END FROM PersonEntity pe WHERE pe.email = ?1";
+
 }
