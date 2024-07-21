1fc50b77ae694b99ca9bab99fd726aa226722de7
Jarmison Paiva <93887813+WebSh4dow@users.noreply.github.com>
2024-07-21 10:09:17 -0300

    Update ClientQuerys.java

--- src/main/java/com/jarmison/dev/api/repository/query/ClientQuerys.java
+++ src/main/java/com/jarmison/dev/api/repository/query/ClientQuerys.java
@@ -2,7 +2,7 @@ package com.jarmison.dev.api.repository.query;
 
 public interface ClientQuerys {
 
-    final String VERIFY_CLIENT_CPF = "SELECT CASE WHEN COUNT(pe) > 0 THEN TRUE ELSE FALSE END FROM PersonEntity pe WHERE pe.email = ?1";
+    final String VERIFY_CLIENT_CPF = "SELECT CASE WHEN COUNT(pe) > 0 THEN TRUE ELSE FALSE END FROM PersonEntity pe WHERE pe.cpf = ?1";
 
     final String VERIFY_CLIENT_EMAIL = "SELECT CASE WHEN COUNT(pe) > 0 THEN TRUE ELSE FALSE END FROM PersonEntity pe WHERE pe.email = ?1";
 
